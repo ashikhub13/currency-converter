627cf8bac02d62e7b4d89972f219938f6ce560bc
ashikhub13 <ashikmohan13@gmail.com>
2022-05-16 01:28:23 +0530

    thymeleaf Integration

--- src/test/java/com/zooplus/converter/controller/CurrencyConverterControllerTest.java
+++ src/test/java/com/zooplus/converter/controller/CurrencyConverterControllerTest.java
@@ -1,9 +1,14 @@
 package com.zooplus.converter.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,6 +17,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.zooplus.converter.entity.CryptoCurrency;
 import com.zooplus.converter.model.CryptoPriceRequest;
@@ -27,23 +34,30 @@ class CurrencyConverterControllerTest {
 	@Mock
 	CurrencyConverterService currencyConverterService;
 
+	@Mock
+	private Model model;
+
 	@Test
 	void testGetCryptoCurrencyList() {
 		List<CryptoCurrency> cryptoCurrencies = new ArrayList<CryptoCurrency>();
 		cryptoCurrencies.add(new CryptoCurrency("BTC", "Bitcoin"));
 		cryptoCurrencies.add(new CryptoCurrency("ETH", "Ether"));
 		when(currencyConverterService.getAllCryptoCurrencies()).thenReturn(cryptoCurrencies);
-		List<CryptoCurrency> cryptoCurrenciesActual = currencyConverterController.getCryptoCurrencyList();
-		Assertions.assertEquals(cryptoCurrencies, cryptoCurrenciesActual);
+		CryptoCurrency cryptoCurrency = new CryptoCurrency(); 
+		CryptoPriceRequest cryptoPriceRequest = new CryptoPriceRequest();
+		String returnValue = currencyConverterController.getCryptoCurrencyList(cryptoCurrency, cryptoPriceRequest, model);
+		verify(model, times(1)).addAttribute("cryptos", cryptoCurrencies);
+        assertEquals("converter", returnValue);
 	}
 
 	@Test
 	void testGetCryptoCurrency() {
-		CryptoPriceRequest CryptoPriceRequest = new CryptoPriceRequest();
-		CryptoPriceRequest.setCode("BTC");
+		CryptoPriceRequest cryptoPriceRequest = new CryptoPriceRequest();
+		cryptoPriceRequest.setCode("BTC");
 		when(currencyConverterService.getPriceAndCurrency(null, "BTC")).thenReturn("€ 124.66");
-		String priceActual = currencyConverterController.getCryptoCurrencyPrice(CryptoPriceRequest);
-		Assertions.assertEquals("€ 124.66", priceActual);
+		String returnValue = currencyConverterController.getCryptoCurrencyPrice(cryptoPriceRequest, model);
+		verify(model, times(1)).addAttribute("message", "€ 124.66");
+		Assertions.assertEquals("converter", returnValue);
 	}
 
 }
